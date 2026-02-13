160448be05cb0aec7e9563997acc6b1aebfb5fb8
sakuraAK <andrey.krutauz@gmail.com>
2026-02-12 20:33:14 -0500

    week 1

---
+++ class-sessions/first.js
@@ -0,0 +1,65 @@
+
+
+console.log("Hello");
+
+let firstName = 'Andrey';
+
+console.log("Hello " + firstName);
+
+let isStudent = true;
+
+
+
+
+`
+Create 2 variables:
+city
+temperature
+
+Print to the screen following
+"It is -6 today in Montreal"
+
+console.log("It is " + temperature ....);
+
+`
+
+
+// let city = 'Montreal';
+// let temperature = -6;
+
+// console.log("It is " + temperature + " today in " + city);
+
+
+let age = 12;
+
+if (age >= 18) {
+    console.log("You can vote!");
+}
+else {
+    console.log("Not yet!");
+}
+
+`
+Create variable temperature
+Write a code that will print on the screen 'It's freezing!'
+if temperature is below zero
+
+`
+let temperature = -6;
+if (temperature <= 0) {
+    console.log("It's freezing!");
+}
+
+
+
+let password = "12345";
+
+//password checker
+
+if (password === "1234") {
+    console.log("Access granted");
+}
+else {
+    console.log("Access denied");
+}
+
