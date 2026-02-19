c87356da9fef0165374d7c638e2a4614b5ad99fd
sakuraAK <andrey.krutauz@gmail.com>
2026-02-19 18:32:04 -0500

    week 1 homework solution

---
+++ homework/week1_sol.js
@@ -0,0 +1,8 @@
+let age = 35;
+let name = "Bill";
+let city = "Montreal";
+
+
+console.log("Hi, my name is " + name + ", I am " + age + " years old and I live in " + city);
+
+console.log(`Hi, my name is ${name}, I am ${age} years old and I live in ${city}.`);
