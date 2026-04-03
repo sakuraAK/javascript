fa9f8802dff7f7d715edbe3d18e1420be145b56e
Andrey <andrey.krutauz@gmail.com>
2026-04-03 08:17:20 -0400

    Add week 6 work in class

--- class-sessions/week6/script.js
+++ class-sessions/week6/script.js
@@ -1,5 +1,22 @@
 console.log('Page loaded successfully!');
 
 document.addEventListener("DOMContentLoaded", function(){
-    
+    let input = document.querySelector("#taskInput");
+    let button = document.querySelector("#addBtn");
+    let taskList = document.querySelector("#taskList");
+
+
+    button.addEventListener('click', function (){
+        let taskName = input.value;
+        
+        if (taskName === undefined || taskName.trim() === '') {
+            return;
+        }
+        let taskElement = document.createElement("li");
+        taskElement.textContent = taskName;
+
+        taskList.appendChild(taskElement);
+
+        input.value = '';
+    });
 });
